a69d185c5ff5074106145db158b9a627091b29f1
yanghy <snxamdf@126.com>
2024-12-18 15:06:50 +0800

    energy build and package

--- .github/shell/install-energy.sh
+++ .github/shell/install-energy.sh
@@ -5,6 +5,7 @@ echo "Install ENERGY CLI"
 cd /app
 
 git clone https://github.com/energye/energy.git
+go env -w GOFLAGS=-buildvcs=false
 cd energy/cmd/energy
 go build -ldflags="-s -w" -o /app/go/bin/
 energy cli -v
